71ca24bef0eb09bbc0688f45fc668ce73db20b48
kyeongin- <a27313797@gmail.com>
2023-09-08 13:10:41 +0900

    Feat: spring boot 기본 환경 세팅

---
+++ backend/src/main/java/com/tjjhtjh/memorise/MemoriseApplication.java
@@ -0,0 +1,13 @@
+package com.tjjhtjh.memorise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriseApplication.class, args);
+	}
+
+}
