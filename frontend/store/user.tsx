053dfe8d780e8100f9b9705cde3b4deda8d5b8f7
김준형 <wnsgud6232@naver.com>
2023-09-20 20:26:42 +0900

    Feat: userSlice 코드 작성 완료

--- frontend/store/user.tsx
+++ frontend/store/user.tsx
@@ -1,9 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+// 타입 지정
+type UserProps = {
+  nickname: string;
+  email: string;
+  profile_img: string;
+};
+
+// 유저 슬라이스
 const userSlice = createSlice({
   name: "user",
-  initialState: {},
-  reducers: {},
+  initialState: {
+    nickname: "",
+    email: "",
+    profile_img: "",
+  },
+  reducers: {
+    setNickname: (state, action: PayloadAction<string>) => {
+      state.nickname = action.payload;
+    },
+    setEmail: (state, action: PayloadAction<string>) => {
+      state.email = action.payload;
+    },
+    setProfileImg: (state, action: PayloadAction<string>) => {
+      state.profile_img = action.payload;
+    },
+  },
 });
 
 export default userSlice.reducer;
+export const { setNickname, setEmail, setProfileImg } = userSlice.actions;
