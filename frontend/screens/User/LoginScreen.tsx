95df11e9788ab0b00c82b92812a2385268cb53c6
김준형 <wnsgud6232@naver.com>
2023-09-20 14:03:48 +0900

    Feat: 카카오 로그인 및 사용자 정보 확인 기능 구현

--- frontend/screens/User/LoginScreen.tsx
+++ frontend/screens/User/LoginScreen.tsx
@@ -9,21 +9,35 @@ import axios from "axios";
 import { styles } from "./LoginStyle";
 
 const LoginScreen = () => {
-  // 카카오 로그인 함수
-  const loginHandler = async () => {
-    await login()
+  // 카카오 사용자 정보 조회 함수
+  const getProfileHandler = () => {
+    getProfile()
       .then((response) => {
-        console.log(response);
+        console.log(response.email);
+        // 백엔드에 이메일 정보 보내서 사용자 확인
+        axios
+          .post("http://j9b106.p.ssafy.io:8000/auth/oauth2/code/kakao", {
+            email: response.email,
+          })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  const getProfileHandler = () => {
-    getProfile()
+  // 카카오 소셜 로그인 함수
+  const loginHandler = async () => {
+    await login()
       .then((response) => {
-        console.log(response);
+        console.log(response.scopes);
+        // 사용자 프로필 정보 조회
+        getProfileHandler();
       })
       .catch((error) => {
         console.log(error);
