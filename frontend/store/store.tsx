c3af775f4d943768e625d9672a15cff19b50e47b
김준형 <wnsgud6232@naver.com>
2023-09-20 20:18:46 +0900

    Feat: user스토어, 리듀서 생성

--- frontend/store/store.tsx
+++ frontend/store/store.tsx
@@ -1,7 +1,13 @@
+// 스토어
 import { configureStore } from "@reduxjs/toolkit";
+
+//리듀서
+import userReducer from "./user";
 
 export const store = configureStore({
   // reduce는 Redux가 사용하는 데이터와
   // 데어터를 변경하는 동작의 상태(state)를 나타내는 슬라이스로 구성
-  reducer: {},
+  reducer: {
+    userInfo: userReducer,
+  },
 });
