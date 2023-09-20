003b97cacb12e7c8f03201a22a04263b32435408
김준형 <wnsgud6232@naver.com>
2023-09-20 15:19:46 +0900

    Feat: 회원여부에 따른 네비게이션 기능 구현

--- frontend/screens/User/LoginScreen.tsx
+++ frontend/screens/User/LoginScreen.tsx
@@ -1,14 +1,24 @@
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
 import { View, Image, Pressable } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
 import KakaoLogins, {
   login,
   getProfile,
 } from "@react-native-seoul/kakao-login";
-import axios from "axios";
+import LinearGradient from "react-native-linear-gradient";
 
 import { styles } from "./LoginStyle";
 
+type RootStackParamList = {
+  SignUp: undefined;
+  Main: undefined;
+};
+
 const LoginScreen = () => {
+  // 네비게이션
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
   // 카카오 사용자 정보 조회 함수
   const getProfileHandler = () => {
     getProfile()
@@ -21,8 +31,10 @@ const LoginScreen = () => {
           .then((response) => {
             if (response.data === true) {
               console.log("가입된 회원입니다.");
+              navigation.navigate("Main");
             } else {
               console.log("회원가입이 필요한 회원입니다.");
+              navigation.navigate("SignUp");
             }
           })
           .catch((error) => {
