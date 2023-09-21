b06516301bc3856b87878594641814b5980c503a
kyeongin- <a27313797@gmail.com>
2023-09-21 22:37:12 +0900

    Feat: bookmark 존재 여부 체크
    - memoSeq와 userSeq에 해당하는 북마크가 있는지 체크하는 로직 추가

--- backend/src/main/java/com/tjjhtjh/memorise/domain/memo/repository/BookmarkRepositoryImpl.java
+++ backend/src/main/java/com/tjjhtjh/memorise/domain/memo/repository/BookmarkRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tjjhtjh.memorise.domain.memo.repository.entity.Bookmark;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.tjjhtjh.memorise.domain.memo.repository.entity.QBookmark.bookmark;
@@ -23,5 +24,11 @@ public class BookmarkRepositoryImpl extends QuerydslRepositorySupport implements
         return queryFactory.selectFrom(bookmark)
                 .where(bookmark.user.email.eq(email).and(bookmark.memo.memoSeq.eq(memoId)))
                 .stream().findAny();
+    }
+
+    @Override
+    public List<Bookmark> bookmarkExistCheck(Long memoId, Long userSeq) {
+        return queryFactory.selectFrom(bookmark).where(bookmark.user.userSeq.eq(userSeq).and(bookmark.memo.memoSeq.eq(memoId)))
+                .fetch();
     }
 }
