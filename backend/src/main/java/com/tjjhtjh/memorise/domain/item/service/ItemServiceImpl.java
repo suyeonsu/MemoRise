3793bebc935a2d10730d7fbadf5bbcc379035721
suyeonsu <agew0423@daum.net>
2023-09-21 21:36:24 +0900

    Fix: Transactional 추가

--- backend/src/main/java/com/tjjhtjh/memorise/domain/item/service/ItemServiceImpl.java
+++ backend/src/main/java/com/tjjhtjh/memorise/domain/item/service/ItemServiceImpl.java
@@ -5,9 +5,11 @@ import com.tjjhtjh.memorise.domain.item.repository.entity.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ItemServiceImpl implements ItemService {
 
     private final ItemRepository itemRepository;
