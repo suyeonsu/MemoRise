71de910f8887346fd367c785a2d01569141402eb
kyeongin- <a27313797@gmail.com>
2023-09-21 22:38:25 +0900

    Refactor: item값 변경
    - 기존에 임시값으로 받던 item을 연관관계 맺은 값으로 변경

--- backend/src/main/java/com/tjjhtjh/memorise/domain/memo/service/dto/response/MemoResponse.java
+++ backend/src/main/java/com/tjjhtjh/memorise/domain/memo/service/dto/response/MemoResponse.java
@@ -1,5 +1,6 @@
 package com.tjjhtjh.memorise.domain.memo.service.dto.response;
 
+import com.tjjhtjh.memorise.domain.item.repository.entity.Item;
 import com.tjjhtjh.memorise.domain.memo.repository.entity.AccessType;
 import com.tjjhtjh.memorise.domain.memo.repository.entity.Memo;
 import lombok.AllArgsConstructor;
@@ -17,16 +18,16 @@ import java.time.LocalDateTime;
 public class MemoResponse {
 
     private String content;
-    private Long itemSeq;
+    private Item item;
     private AccessType accessType;
     private String userId;
     private LocalDateTime updatedAt;
     private boolean bookmark;
 
-    public MemoResponse response(Memo memo) {
+    public MemoResponse response(Memo memo , Item item1) {
         return MemoResponse.builder()
                 .content(memo.getContent())
-                .itemSeq(memo.getItemSeq())
+                .item(item1)
                 .accessType(memo.getAccessType())
                 .updatedAt(memo.getUpdatedAt())
                 .userId(memo.getUser().getEmail())
