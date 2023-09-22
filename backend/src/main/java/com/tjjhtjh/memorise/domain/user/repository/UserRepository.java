2291ad065528b11f30df0d0be038f6d0070d01aa
김수연 <agew0423@daum.net>
2023-09-22 17:01:33 +0900

    Feat: 유저 목록 조회 S09P22B106-341

--- backend/src/main/java/com/tjjhtjh/memorise/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/tjjhtjh/memorise/domain/user/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserSupportRepository {
     Optional<User> findByUserSeqAndIsDeletedFalse(Long userSeq);
     Optional<User> findByEmailAndIsDeletedFalse(String email);
     Optional<User> findByEmail(String email);
