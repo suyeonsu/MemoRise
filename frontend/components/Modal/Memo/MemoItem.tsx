9b5a498f10afc7dcecf178e7e8caafa079121869
김준형 <wnsgud6232@naver.com>
2023-09-22 15:15:43 +0900

    Style: 메모 모달 기본 틀 작성

--- frontend/components/Modal/Memo/MemoItem.tsx
+++ frontend/components/Modal/Memo/MemoItem.tsx
@@ -1,4 +1,9 @@
+// 라이브러리
 import { View, Text, StyleSheet, Dimensions } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+
+// 스타일
+import { styles } from "../../../screens/Main/MainStyle";
 
 import { calculateDynamicWidth } from "../../../constants/dynamicSize";
 
@@ -7,10 +12,26 @@ const screenHeight = Dimensions.get("window").height;
 
 const MemoItem = () => {
   return (
-    <View>
-      <Text>메모 모달</Text>
+    <View style={styles.memoContainer}>
+      <View style={tempstyles.memoSize}>
+        <LinearGradient
+          colors={["#FFFFFF", "#F5F5F5"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          style={{ flex: 1 }}
+        >
+          <Text>메모 모달</Text>
+        </LinearGradient>
+      </View>
     </View>
   );
 };
 
 export default MemoItem;
+
+const tempstyles = StyleSheet.create({
+  memoSize: {
+    width: calculateDynamicWidth(306),
+    height: calculateDynamicWidth(306),
+  },
+});
