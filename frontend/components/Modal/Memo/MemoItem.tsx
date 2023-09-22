e24d2d1529a7bc5a6d6b2bc3e968fd547799e978
김준형 <wnsgud6232@naver.com>
2023-09-22 16:03:08 +0900

    Feat: 북마크, 날짜 기능 구현

--- frontend/components/Modal/Memo/MemoItem.tsx
+++ frontend/components/Modal/Memo/MemoItem.tsx
@@ -1,5 +1,12 @@
 // 라이브러리
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  Pressable,
+} from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 
 // 스타일
@@ -12,18 +19,26 @@ const screenHeight = Dimensions.get("window").height;
 
 const MemoItem = () => {
   return (
-    <View style={styles.memoContainer}>
-      <View style={tempstyles.memoSize}>
-        <LinearGradient
-          colors={["#FFFFFF", "#F5F5F5"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 0, y: 1 }}
-          style={{ flex: 1 }}
-        >
-          <Text>메모 모달</Text>
-        </LinearGradient>
+    <>
+      <View style={styles.memoContainer}>
+        <View style={tempstyles.memoSize}>
+          <LinearGradient
+            colors={["#FFFFFF", "#F5F5F5"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+            style={{ flex: 1 }}
+          >
+            <Text style={tempstyles.calendar}>2023. 09. 22</Text>
+          </LinearGradient>
+        </View>
       </View>
-    </View>
+      <Pressable>
+        <Image
+          source={require("../../../assets/icons/bookmarkblue.png")}
+          style={tempstyles.bookmarkSize}
+        />
+      </Pressable>
+    </>
   );
 };
 
@@ -33,5 +48,19 @@ const tempstyles = StyleSheet.create({
   memoSize: {
     width: calculateDynamicWidth(306),
     height: calculateDynamicWidth(306),
+  },
+
+  bookmarkSize: {
+    width: calculateDynamicWidth(17),
+    height: calculateDynamicWidth(23),
+    marginLeft: calculateDynamicWidth(50),
+    bottom: calculateDynamicWidth(520),
+    position: "absolute",
+  },
+
+  calendar: {
+    top: calculateDynamicWidth(9),
+    marginLeft: calculateDynamicWidth(225),
+    color: "#CCCCCC",
   },
 });
