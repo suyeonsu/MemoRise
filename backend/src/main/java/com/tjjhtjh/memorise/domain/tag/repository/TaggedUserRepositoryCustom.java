003a931a67ed6833bfb81f1c5b0373b73f86c423
kyeongin- <a27313797@gmail.com>
2023-09-22 19:46:36 +0900

    Feat: userTag repository 생성
    - memoId로 tagList 반환하는 함수 작성

---
+++ backend/src/main/java/com/tjjhtjh/memorise/domain/tag/repository/TaggedUserRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.tjjhtjh.memorise.domain.tag.repository;
+
+import com.tjjhtjh.memorise.domain.tag.repository.entity.TaggedUser;
+
+import java.util.List;
+
+public interface TaggedUserRepositoryCustom {
+    List<TaggedUser> findByTaggedUserWithMemoSeq(Long memoId);
+}
