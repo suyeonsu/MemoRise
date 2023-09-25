51aac0f5fb66d326cdd0b096d92255898f787874
김수연 <agew0423@daum.net>
2023-09-25 16:27:45 +0900

    Fix: 내 그룹 목록 조회에서 비밀그룹 여부 반환을 추가

--- backend/src/main/java/com/tjjhtjh/memorise/domain/team/service/dto/response/MyTeamListResponse.java
+++ backend/src/main/java/com/tjjhtjh/memorise/domain/team/service/dto/response/MyTeamListResponse.java
@@ -16,6 +16,7 @@ public class MyTeamListResponse {
     private boolean isOwner;
     private String ownerProfile;
     private List<String> memberProfiles;
+    private boolean isPassword;
 
     public MyTeamListResponse(Team team, User user, List<String> memberProfiles) {
         this.teamSeq = team.getTeamSeq();
@@ -24,5 +25,6 @@ public class MyTeamListResponse {
         this.isOwner = team.getOwner().equals(user.getUserSeq());
         this.ownerProfile = isOwner ? null : memberProfiles.get(0);
         this.memberProfiles = isOwner ? memberProfiles : memberProfiles.subList(1, memberProfiles.size());
+        this.isPassword = team.getPassword() != null;
     }
 }
