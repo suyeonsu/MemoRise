d2ba4f3b6542ed95cb8250d10957584f106c1467
김준형 <wnsgud6232@naver.com>
2023-09-25 10:25:37 +0900

    Feat: 메모 수정/삭제를 위한 함수 생성

--- frontend/components/Modal/Memo/MemoDetail.tsx
+++ frontend/components/Modal/Memo/MemoDetail.tsx
@@ -82,6 +82,12 @@ const MemoDetail = () => {
     setIsBookMark(!isBookMark);
   };
 
+  // 메모 수정 함수
+  const updateMemoHandler = () => {};
+
+  // 메모 삭제 함수
+  const deleteMemoHandler = () => {};
+
   return (
     <>
       <View style={detailStyle.mainContainer}>
@@ -98,13 +104,13 @@ const MemoDetail = () => {
                   2023. 09. 24 오전 12:50
                 </Text>
                 <View style={detailStyle.iconContainer}>
-                  <Pressable>
+                  <Pressable onPress={updateMemoHandler}>
                     <Image
                       source={require("../../../assets/icons/update.png")}
                       style={detailStyle.icon}
                     />
                   </Pressable>
-                  <Pressable>
+                  <Pressable onPress={deleteMemoHandler}>
                     <Image
                       source={require("../../../assets/icons/delete.png")}
                       style={detailStyle.icon}
