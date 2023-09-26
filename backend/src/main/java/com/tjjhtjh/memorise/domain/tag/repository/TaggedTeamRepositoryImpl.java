b972242c4db8fc06bbbd9546e10a586487d6bd2b
kyeongin- <a27313797@gmail.com>
2023-09-26 17:48:20 +0900

    Refactor: 메모 상세조회시 태그된 team name 리스트 함께 가져오도록 수정

--- backend/src/main/java/com/tjjhtjh/memorise/domain/tag/repository/TaggedTeamRepositoryImpl.java
+++ backend/src/main/java/com/tjjhtjh/memorise/domain/tag/repository/TaggedTeamRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.tjjhtjh.memorise.domain.tag.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tjjhtjh.memorise.domain.tag.repository.entity.TaggedTeam;
+import com.tjjhtjh.memorise.domain.tag.service.dto.response.TaggedTeamResponse;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
@@ -18,7 +20,13 @@ public class TaggedTeamRepositoryImpl extends QuerydslRepositorySupport implemen
     }
 
     @Override
-    public List<TaggedTeam> findByTaggedTeamWithMemoSeq(Long memoId) {
-        return queryFactory.selectFrom(taggedTeam).where(taggedTeam.memo.memoSeq.eq(memoId)).fetch();
+    public List<TaggedTeam> findByTaggedTeamWithMemoSeq(Long memoSeq) {
+        return queryFactory.selectFrom(taggedTeam).where(taggedTeam.memo.memoSeq.eq(memoSeq)).fetch();
+    }
+
+    @Override
+    public List<TaggedTeamResponse> findByTaggedTeamList(Long memoSeq) {
+        return queryFactory.select(Projections.fields(TaggedTeamResponse.class,taggedTeam.team.name))
+                .from(taggedTeam).where(taggedTeam.memo.memoSeq.eq(memoSeq)).fetch();
     }
 }
--- backend/src/main/java/com/tjjhtjh/memorise/domain/tag/service/dto/response/TaggedTeamResponse.java
+++ backend/src/main/java/com/tjjhtjh/memorise/domain/tag/service/dto/response/TaggedTeamResponse.java
@@ -1,4 +1,22 @@
 package com.tjjhtjh.memorise.domain.tag.service.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TaggedTeamResponse {
+
+    private String name;
+
+    public TaggedTeamResponse teamNameResponse(String teamName) {
+        return TaggedTeamResponse.builder()
+                .name(teamName)
+                .build();
+    }
+
 }
