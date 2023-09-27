8652c92ae514a1e0ad91d74cf5bebfba884e4dd9
김준형 <wnsgud6232@naver.com>
2023-09-27 15:45:30 +0900

    Feat: 북마크 메모리스트 가져오는 통신 코드 작성

--- frontend/screens/MenuContents/Memo/SavedMemoScreen.tsx
+++ frontend/screens/MenuContents/Memo/SavedMemoScreen.tsx
@@ -1,10 +1,52 @@
+// 라이브러리
+import axios from "axios";
+import React, { useEffect } from "react";
 import { View, Text, Image } from "react-native";
+import { useSelector } from "react-redux";
 import LinearGradient from "react-native-linear-gradient";
 
+// 컴포넌트
 import GoBackHeader from "../../../components/Header/GoBackHeader";
+import { RootState } from "../../../store/store";
+
+// 스타일
 import { styles } from "./MemoStyle";
 
+// 통신
+import { BACKEND_URL } from "../../../util/http";
+
 const SavedMemoScreen = () => {
+  // 타입
+  type BookMarkMemoProps = {
+    memoSeq: number;
+    content: string;
+    accessType: string;
+    nickname: string;
+    updateAt: string;
+    file: string | null;
+    itemImage: string | null;
+    isBookmarked: boolean;
+  };
+
+  // 유저 ID
+  const userId = useSelector((state: RootState) => state.userInfo.id);
+
+  // 백엔드 통신
+  useEffect(() => {
+    console.log(userId);
+    const fetchData = async () => {
+      await axios
+        .get(BACKEND_URL + `/user/${userId}/bookmarks`)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchData();
+  }, []);
+
   return (
     <LinearGradient
       // colors={["#F5F5F5", "red"]}
