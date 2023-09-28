ce2d14c55556e5222d187aaeb53d414d2438514a
김준형 <wnsgud6232@naver.com>
2023-09-29 02:27:32 +0900

    Feat: 북마크 버튼 하드코딩 처리

--- frontend/components/Button/BookMarkBtn.tsx
+++ frontend/components/Button/BookMarkBtn.tsx
@@ -2,6 +2,10 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Pressable, Image } from "react-native";
+import { useSelector } from "react-redux";
+
+//컴포넌트
+import { RootState } from "../../store/store";
 
 // 통신
 import { BACKEND_URL } from "../../util/http";
@@ -18,17 +22,27 @@ const BookMarkBtn: React.FC<BookMarkProps> = ({
   detailStyle,
   bookmarkType,
 }) => {
+  // 유저ID
+  const userId = useSelector((state: RootState) => state.userInfo.id);
+
+  // 북마크 상태관리
   const [isBookMark, setIsBookMark] = useState<boolean>(bookmarkType);
 
   const changeIsBookMark = (id: number) => {
     if (isBookMark) {
-      axios.delete(BACKEND_URL + `/memos/${id}/bookmarks/23`).catch((error) => {
-        console.error(error);
-      });
+      // axios.delete(BACKEND_URL + `/memos/${id}/bookmarks/${userId}`)
+      axios
+        .delete(BACKEND_URL + `/memos/${id}/bookmarks/23`) // 쫀듸기
+        .catch((error) => {
+          console.error(error);
+        });
     } else {
-      axios.post(BACKEND_URL + `/memos/${id}/bookmarks/23`).catch((error) => {
-        console.error(error);
-      });
+      // axios.post(BACKEND_URL + `/memos/${id}/bookmarks/${userId}`)
+      axios
+        .post(BACKEND_URL + `/memos/${id}/bookmarks/23`) // 쫀듸기
+        .catch((error) => {
+          console.error(error);
+        });
     }
     setIsBookMark(!isBookMark);
   };
