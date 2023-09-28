636acef9f68f0a1e8f9db95e29804773be5df181
김준형 <wnsgud6232@naver.com>
2023-09-28 15:55:16 +0900

    Feat: 로딩 컴포넌트 작성

--- frontend/components/LoadingOverlay.tsx
+++ frontend/components/LoadingOverlay.tsx
@@ -1,13 +1,19 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 
 const LoadingOverlay = () => {
   return (
-    <View>
-      <Text>컴포넌트 기본</Text>
+    <View style={styles.Container}>
+      <ActivityIndicator size="large" color="#2C2C2C" />
     </View>
   );
 };
 
 export default LoadingOverlay;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
